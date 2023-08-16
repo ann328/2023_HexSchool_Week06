809460b3263b8bd16ca43bb2fd181b9712bd5e6d
“ann” <136979138+ann328@users.noreply.github.com>
2023-08-16 18:19:06 +0800

    feat: 🎸 add single_event_page

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig({
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
-    open: "pages/index.html",
+    open: "pages/single_event_page.html",
   },
   build: {
     rollupOptions: {
